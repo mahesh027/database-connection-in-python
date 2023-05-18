bd4feebdde8e3331aa2f0eb74b46acdaf28e7821
K Mahesh <mahesh0352002@gmail.com>
2023-05-18 17:23:51 +0530

    final commit

--- db.py
+++ db.py
@@ -1,34 +1,62 @@
 import sqlite3
 con=sqlite3.connect('users.db')
 
-def insertData(name,age,city):  #id variable not included as it is in autoincremental mode
-    qry="insert into users (NAME,AGE,CITY) values (?,?,?)"
+def insertData(name,age,city):
+    qry="insert into users (NAME,AGE,CITY) values (?,?,?);"
     con.execute(qry,(name,age,city))
     con.commit()
-    print("user details added")
+    print("User Details Added")
+    
+def updateData(name,age,city,id):
+    qry="update users set NAME=?,AGE=?,CITY=? where id=?;"
+    con.execute(qry,(name,age,city,id))
+    con.commit()
+    print("User Details Updated")
+    
+def deleteData(id):
+    qry="delete from users where id=?;"
+    con.execute(qry,(id))
+    con.commit()
+    print("User Details Deleted")
 
+def selectData():
+    qry="select * from users"
+    result=con.execute(qry)
+    for row in result:
+        print(row)
+    
 print("""
 1.Insert
 2.Update
 3.Delete
 4.Select
 """)
+
 ch=1
-while ch==1 :
-    c=int(input("select your choice"))
+while ch==1:
+    c=int(input("Select Your Choice : "))
     if(c==1):
-       print("Add new Record")
-       name=input("enter the name")
-       age=int(input("enter age"))
-       city=input("enter the city")
-    elif(c==2):
-       print("edit a record")
-    elif(c==3):
-       print("Delete a record")
-    elif(c==4):
-       print("print all record")
+        print("Add New Record")
+        name=input("Enter Name : ")
+        age=input("Enter Age : ")
+        city=input("Enter City : ")
+        insertData(name,age,city)
+    elif (c==2):
+        print("Edit A Record")
+        id=input("Enter ID : ")
+        name=input("Enter Name : ")
+        age=input("Enter Age : ")
+        city=input("Enter City : ")
+        updateData(name,age,city,id)
+    elif (c==3):
+        print("Delete A Record")
+        id=input("Enter ID : ")
+        deleteData(id)
+    elif (c==4):
+        print("Print All Record")
+        selectData()
     else:
-       print("invalid selection")
-    ch=int(input("Enter 1 to continue"))
-print("exited")
+        print("Invalid Selectio")
+    ch=int(input("Enter 1 To Continue : "))
+print("Thank You")
     
